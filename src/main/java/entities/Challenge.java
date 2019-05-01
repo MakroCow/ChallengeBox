be388d57414a2c02b58a767cd5823926e9cffe67
MakroCow <yoco.lie@gmail.com>
2019-05-01 22:00:54 +0200

    Added example queries and api-endpoints. Entities are not yet well annotated to produce well formed database output

--- src/main/java/entities/Challenge.java
+++ src/main/java/entities/Challenge.java
@@ -4,6 +4,10 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "findAllChallenges", query = "SELECT c FROM Challenge c"),
+        @NamedQuery(name = "findChallengeByID", query = "SELECT c FROM Challenge c WHERE c.id = :id")}
+)
 public class Challenge {
 
     @Id
@@ -11,24 +15,60 @@ public class Challenge {
     public int id;
 
     @Column
-    private String taste;
+    private String titel;
 
     @Column
     private String description;
 
-    @Column
-    private String titel;
 
-    @OneToMany(fetch = FetchType.LAZY)
+    /*@OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id")
     @OrderColumn
-    private List<Tag> tags;
+    private List<Tag> tags;*/
+
+    //@Column
+    //private ... picture;
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+/*    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }*/
+
+
+
 
     public int getId() {
         return id;
     }
 
+    public String getTitle() {
+        return titel;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String toString() {
+        return id + " " + titel + " " + description;
     }
 }
--- src/main/java/services/HelloWorldController.java
+++ src/main/java/services/HelloWorldController.java
@@ -1,13 +1,65 @@
 package services;
 
+import entities.Challenge;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Path("/helloworld")
 public class HelloWorldController {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    @Path("/1")
     @GET
-    public String helloWorld() {
-        return "Hello World";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Challenge> helloWorld1() {
+        Query query = em.createNamedQuery("findAllChallenges");
+        List<Challenge> challenges = query.getResultList();
+        //return challenges.stream().map(Challenge::toString).collect(Collectors.joining(", "));
+        return challenges;
+    }
+
+    @Path("/2/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Challenge> helloWorld2(@PathParam("id") int id) {
+        Query query = em.createNamedQuery("findChallengeByID");
+        query.setParameter("id", id);
+        List<Challenge> challenges = query.getResultList();
+        return challenges;
+    }
+
+    @Path("/fillWithExampleData")
+    @GET
+    @Transactional
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Challenge> fillWithExampleData() {
+
+        String[] challengetitels = {"Laufen", "Gehen", "Sprechen", "Schreien", "Kochen", "Kennenlernen"};
+        String[] challgedescriptions = {"Gehe heute 10 Minuten länger laufen, als letztesmal", "Gehe nach dem Abendessen noch eine Runde und versuche dabei ein Tier zu sichten", "Sprich deinen Schwarm auf seine Frisur an - Sei es positiv oder negativ", "Kletter auf den höchsten Punkt im 2km Umkreis und schreie einen Tarzan Schrei", "Backe heute einen Kuchen mit mind. 2 grünen Zutaten", "Frag jemanden anch seiner Handynummer"};
+
+        for (int i = 0; i < challengetitels.length; i++) {
+            Challenge c = new Challenge();
+            c.setTitel(challengetitels[i]);
+            c.setDescription(challgedescriptions[i]);
+            em.persist(c);
+        }
+
+        Query query = em.createNamedQuery("findAllChallenges");
+        List<Challenge> challenges = query.getResultList();
+        //return challenges.stream().map(Challenge::toString).collect(Collectors.joining(", "));
+        return challenges;
     }
 }
